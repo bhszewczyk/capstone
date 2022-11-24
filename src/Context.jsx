b1a1d48fd1de14dc28470aaf818ed21879af484f
Barbara Szewczyk <comamysza@gmail.com>
2022-11-24 12:38:00 +0100

    Adds Context and ContextProvider

---
+++ src/Context.jsx
@@ -0,0 +1,20 @@
+import { createContext, useEffect, useState } from 'react';
+
+const Context = createContext();
+
+const URL_PHOTOS =
+	'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json';
+
+function ContextProvider({ children }) {
+	const [photos, setPhotos] = useState([]);
+
+	useEffect(() => {
+		fetch(URL_PHOTOS)
+			.then((res) => res.json())
+			.then((data) => setPhotos(data));
+	}, [photos]);
+
+	return <Context.Provider value={{ photos }}>{children}</Context.Provider>;
+}
+
+export { Context, ContextProvider };
