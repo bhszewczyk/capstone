840cc2b6518235171335cf0e2843e4ead2a05902
Barbara Szewczyk <comamysza@gmail.com>
2022-11-24 12:38:07 +0100

    Fetched photo data

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { ContextProvider } from './Context';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<ContextProvider>
+			<Router>
+				<App />
+			</Router>
+		</ContextProvider>
 	</React.StrictMode>
 );
