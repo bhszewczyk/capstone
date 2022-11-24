d86285770bf33089d7a7e5ab756e9a80e5f0620e
Barbara Szewczyk <comamysza@gmail.com>
2022-11-24 15:45:46 +0100

    Adds PropTypes to Image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
 import { Context } from '../Context';
 
 function Image({ className, image }) {
@@ -26,5 +27,14 @@ function Image({ className, image }) {
 		</div>
 	);
 }
+
+Image.propTypes = {
+	className: PropTypes.string,
+	image: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		url: PropTypes.string.isRequired,
+		isFavorite: PropTypes.bool,
+	}),
+};
 
 export default Image;
