b1d209f2a8c7027982a1cd8c76c3057dc0bac7fc
Barbara Szewczyk <comamysza@gmail.com>
2022-11-27 13:55:06 +0100

    Creates CartItem component with delete option

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { Context } from '../Context';
+
+function CartItem({ item }) {
+	const { removeImageFromCart } = useContext(Context);
+	return (
+		<div className='cart-item'>
+			<i
+				className='ri-delete-bin-line'
+				onClick={() => removeImageFromCart(item)}
+			></i>
+			<img src={item.url} alt='Product' width='130px' />
+			<p>$5.99</p>
+		</div>
+	);
+}
+
+export default CartItem;
